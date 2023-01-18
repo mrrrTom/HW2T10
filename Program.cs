6fcda8708612e391e8ef58d17aafbc5b3319108c
Daniil Gorbunov <dagoroff@gmail.com>
2023-01-19 02:55:58 +0600

    Added solution for task 10

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Welcome to the second number finder!");
+Console.WriteLine("Please, insert three-digit number:");
+var input = Console.ReadLine();
+var number = default(int);
+if (!int.TryParse(input, out number))
+{
+    Console.WriteLine("Sorry, inserted value is not an integer... Bye!");
+    return;
+}
+
+if ((number < 0 && input?.Length == 4) || (number > 0 && input?.Length == 3))
+{
+    Console.WriteLine($"{(number/10) % 10}".Last());
+}
+else
+{
+    Console.WriteLine("Sorry, inserted number is not three-digit... Bye!");
+    return;
+}
